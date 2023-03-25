5149f584d133da231d45f1158a9cfdda8e480427
gabinbusssonneau <gabinbussonneau@gmail.com>
2023-03-25 09:56:17 +0100

    object version

--- main.c
+++ main.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
 
-int liste[9][9];
+#define ROWS 9
+#define COLS 9
 
-int move(int liste[9][9], char move)
-    {
-        switch(move){
-            case 'z':
-                printf("haut");
-                break;
-            case 'q':
-                printf("gauche");
-                break;
-            case 's':
-                printf("bas");
-                break;
-            case 'd':
-                printf("droite");
-                break;
-            default:
-                printf("erreur");
+// Définition de la classe Board
+typedef struct {
+    int grid[ROWS][COLS];
+} Board;
+
+// Initialisation du plateau avec la valeur 1
+void init_board(Board *board) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            board->grid[i][j] = 1;
         }
-
     }
+}
+void printboard(Board *board) {
+    printf("    ");
+    for(int i = 0; i < ROWS; i++) {
+        printf("%d  ", i + 1);
+    }
+    printf("\n  +");
 
-int level1(int liste[9][9]){
-    int level[9][9] = {
+    for(int i = 0; i < 7; i++) {
+        printf("---+");
+    }
+    printf("\n");
+
+    for(int i = 0; i < ROWS; i++) {
+        printf("%d |", i + 1);
+        for(int j = 0; j < COLS; j++) {
+            if (board->grid[i][j] == 0) {
+                printf(" . ");
+            }
+            else {
+                printf(" %d ", board->grid[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+// Initialisation du niveau 1 du jeu
+void init_level1(Board *board) {
+    int level[ROWS][COLS] = {
             {1, 1, 3, 1, 1, 1, 1, 1, 1},
             {1, 2, 1, 1, 1, 1, 1, 1, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -35,66 +55,58 @@ int level1(int liste[9][9]){
             {1, 1, 1, 1, 1, 1, 1, 3, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
-    for(int i=0; i<9;i++){
-        for(int j=0; j<9;j++){
-            liste[i][j] = level[i][j];
 
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            board->grid[i][j] = level[i][j];
         }
     }
-    liste[8][5]=0;
-}
-int initboard(int liste[9][9]){
-    for(int i=0; i<9;i++){
-        for(int y=0; y<9;y++){
-            liste[i][y]=1;
-        }
-    }
+
+    // La position (8, 5) est libre
+    board->grid[8][5] = 0;
 }
 
-
-void printboard(int liste[9][9]){
-    printf("    ");
-
-    for(int i=0; i<9;i++){
-        printf("%d  ",i+1);
-    }
-    printf("\n  +");
-
-    for(int i=0; i<7;i++){
-        printf("---+");
-    }
-    printf("\n");
-
-    for(int i=0;i<9;i++){
-        printf( "%d |", i+1);
-        for(int j=0; j<9;j++){
-            if (liste[i][j]==0){
-                printf(" . ");
-            }
-            else{
-                printf(" %d ", liste[i][j]);
-            }
-
-        }
-        printf("\n");
-
+// Déplacement du joueur sur le plateau
+void move(Board *board, char direction) {
+    switch(direction) {
+        case 'z':
+            printf("haut\n");
+            break;
+        case 'q':
+            printf("gauche\n");
+            break;
+        case 's':
+            printf("bas\n");
+            break;
+        case 'd':
+            printf("droite\n");
+            break;
+        default:
+            printf("erreur\n");
     }
 }
+
 int main() {
-    int liste[9][9];
-    char deplacement='0';
-    printf("Welcolme to cardinal game!\n");
-    initboard(liste);
-    printboard(liste);
-    printf("\n manche 0\n");
-    level1(liste);
-    printboard(liste);
-    printf("\n manche 1\n");
-    printf("Les deplacements :\n pour aller en haut -> z \n pour aller en bas -> s \n pour aller a gauche -> q \n pour aller a droite -> d");
-    printf("\n votre choix ? : ");
-    for(int i=0; i<5;i++){
-        scanf("%s",&deplacement);
-        move(liste,deplacement);
+    char direction;
+    printf("Bienvenue au jeu Cardinal!\n");
+
+    Board board;
+    init_board(&board);
+
+    printf("\nManche 0\n");
+    printboard(board.grid);
+
+    init_level1(&board);
+
+    printf("\nManche 1\n");
+    printboard(board.grid);
+
+    printf("Les deplacements :\n pour aller en haut -> z \n pour aller en bas -> s \n pour aller a gauche -> q \n pour aller a droite -> d\n");
+    printf("Votre choix ? : ");
+
+    for (int i = 0; i < 5; i++) {
+        scanf(" %c", &direction);
+        move(&board, direction);
     }
 
     return 0;
