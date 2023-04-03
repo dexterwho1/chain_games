240816455aad5344ec58adf7bdb969dff712aca4
gabinbusssonneau <101792230+dexterwho1@users.noreply.github.com>
2023-04-03 10:59:17 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,51 +1,88 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <Windows.h>
+#include "main.h"
 
-#define ROWS 9
-#define COLS 9
-
-// Définition de la classe Board
-typedef struct {
-    int grid[ROWS][COLS];
-    int casetrouver[95][2];
-
-} Board;
+int memoire[89][3];
+int indice=0;
 
 
 
 
 
-void setzero(Board *board, int x, int y, int n) {
-    board->casetrouver[n][0] = x;
-    board->casetrouver[n][1] = y;
-    board->casetrouver[n][3] = board->grid[x][y];
 
-    board->grid[x][y] = 0;
-
-    if(board->casetrouver[n][3] >board->grid[x][y]){
-    printf("%d plus grand que %d",board->casetrouver[n][3],board->casetrouver[n-1][3]);
+void mettreToutAZero(Board *board) {
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            board->GRID[i][j] = 0;
         }
-    else if(board->casetrouver[n][3] < board->grid[x][y]){
-        printf("%d plus petit que %d",board->casetrouver[n][3],board->casetrouver[n-1][3]);
-
-
     }
-
-    //printf("votre %d coup est %d ",n,board->casetrouver[n][3]);
-
 }
 
-
-// Initialisation du plateau avec la valeur 1
 void init_board(Board *board) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            board->grid[i][j] = 1;
+    board->N=0;
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            board->GRID[i][j] = 1;
         }
     }
 }
+
+bool estPartieTerminee(Board *board) {
+    bool continuepartie= true;
+    int casesNonNulles = 0;
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            if (board->GRID[i][j] > 0) {
+                casesNonNulles++;
+                if (casesNonNulles > 0) {
+                    return continuepartie=false;
+                }
+            }
+        }
+    }
+
+    return continuepartie;
+}
+
+void printboardwithpos(Board *board, int x, int y) {
+    printf("    ");
+    for(int i = 0; i < X; i++) {
+        printf("%d  ", i + 1);
+    }
+    printf("\n  +");
+
+    for(int i = 0; i < X; i++) { // Change the loop limit to X instead of 7
+        printf("---+");
+    }
+    printf("\n");
+
+    for(int i = 0; i < X; i++) {
+        printf("%d |", i + 1);
+        for(int j = 0; j < Y; j++) {
+            if (i == x && j == y) {
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+                printf(" %d ", board->GRID[i][j]);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+            }
+            else if (board->GRID[i][j] == 0) {
+                printf(" . ");
+            }
+            else {
+                printf(" %d ", board->GRID[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+
 void printboard(Board *board) {
     printf("    ");
-    for(int i = 0; i < ROWS; i++) {
+    for(int i = 0; i < X; i++) {
         printf("%d  ", i + 1);
     }
     printf("\n  +");
@@ -55,157 +92,218 @@ void printboard(Board *board) {
     }
     printf("\n");
 
-    for(int i = 0; i < ROWS; i++) {
+    for(int i = 0; i < X; i++) {
         printf("%d |", i + 1);
-        for(int j = 0; j < COLS; j++) {
-            if (board->grid[i][j] == 0) {
+        for(int j = 0; j < Y; j++) {
+            if (board->GRID[i][j] == 0) {
                 printf(" . ");
             }
             else {
-                printf(" %d ", board->grid[i][j]);
+                printf(" %d ", board->GRID[i][j]);
             }
         }
         printf("\n");
     }
 }
 
-// Initialisation du niveau 1 du jeu
-void init_level1(Board *board) {
+void init_level1(Board*board){
+    int level[X][Y] = {
 
-    int level[ROWS][COLS] = {
-            {5, 9, 3, 1, 8, 4, 7, 6, 2},
-            {4, 2, 9, 1, 5, 7, 8, 3, 6},
-            {6, 7, 1, 4, 9, 2, 8, 5, 3},
-            {2, 5, 7, 3, 1, 9, 4, 8, 6},
-            {3, 1, 6, 5, 2, 8, 9, 7, 4},
-            {8, 4, 2, 6, 7, 1, 3, 9, 5},
-            {9, 8, 4, 2, 3, 6, 1, 5, 7},
-            {7, 6, 5, 8, 4, 3, 2, 1, 9},
-            {1, 3, 8, 9, 6, 0, 4, 2, 7}
+            {9},
+            {7, },
+            {1},
     };
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            board->GRID[i][j] = level[i][j];
+        }
+    }
+    memoire[indice][0] = 2;
+    memoire[indice][1] = 0;
+    memoire[indice][2] = 0;
+    board->GRID[0][2] = 0;
+    board->CASETROUVER[board->N][0] = 0;
+    board->CASETROUVER[board->N][1] = 2;
+    board->CASETROUVER[board->N][2] = 0;
 
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            board->grid[i][j] = level[i][j];
+
+}
+
+void init_level2(Board*board){
+    int level[X][Y] = {
+
+            {5},
+            {6, },
+            {1},
+    };
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            board->GRID[i][j] = level[i][j];
+        }
+    }
+    memoire[indice][0] = 2;
+    memoire[indice][1] = 0;
+    memoire[indice][2] = 0;
+    board->GRID[0][2] = 0;
+    board->CASETROUVER[board->N][0] = 0;
+    board->CASETROUVER[board->N][1] = 2;
+    board->CASETROUVER[board->N][2] = 0;
+
+
+}
+
+void demandeDeplacement(Board *board) {
+    char direction='z';
+    int n=board->N;
+    n=n-1;
+    int x=memoire[indice][0];
+    int y=memoire[indice][1];
+    int value=board->CASETROUVER[n][2];
+    scanf("%s",&direction);
+
+    board->N++;
+
+    if(direction=='z'){
+        //haut
+        printf("actuellement %d  et %d ",memoire[indice][2],board->GRID[x-1][y]);
+        if(memoire[indice][2]<board->GRID[x-1][y]){
+            printf("plus petit");
+            indice++;
+
+            memoire[indice][0]=x-1;
+            memoire[indice][1]=y;
+            memoire[indice][2]=board->GRID[x-1][y];
+            board->GRID[x][y]=0;
+        }
+        else{
+            printf("plus grand");
+        }
+
+
+    }
+
+    if(direction=='r'){
+        if (indice > 0) {
+            board->GRID[x][y] = memoire[indice][2];
+            indice--;
+            x = memoire[indice][0];
+            y = memoire[indice][1];
+        } else {
+            printf("Impossible de revenir en arrière.\n");
         }
     }
 
-    // La position (8, 5) est libre
-    board->grid[8][5] = 0;
-    setzero(board, 8,5,0);
+    if(direction=='s'){
+        if (memoire[indice][2] < board->GRID[x+1][y]) {
+            printf("plus petit");
+            indice++;
+            memoire[indice][0]=x+1;
+            memoire[indice][1]=y;
+            memoire[indice][2]=board->GRID[x+1][y];
+            board->GRID[x][y]=0;
 
-}
+        } else {
+            printf("%d plus ppetit que l'erreur %d",memoire[indice][2] , board->GRID[x+1][y]);
+        }
 
-// Déplacement du joueur sur le plateau
-// Déplacement du joueur sur le plateau
-// Déplacement du joueur sur le plateau
-
-int verifierordrecroissant(Board*board,int n){
-    return 0;
+    }
+    if(direction == 'm'){
+        mettreToutAZero(board);
+    }
 
 
-}
+    if(direction=='q'){
+        //gauche
+        printf("actuellement %d  et %d ",memoire[indice][2],board->GRID[x][y-1]);
+        if(memoire[indice][2]<board->GRID[x][y-1]){
+            printf("plus petit");
+            indice++;
 
-void haut(Board *board, int n) {
-    verifierordrecroissant(board,n);
-    board->casetrouver[n][0]=board->casetrouver[n-1][0]-1;
-    board->casetrouver[n][1]=board->casetrouver[n-1][1];
-}
+            memoire[indice][0]=x;
+            memoire[indice][1]=y-1;
+            memoire[indice][2]=board->GRID[x][y-1];
+            board->GRID[x][y]=0;
 
-void gauche(Board *board, int n) {
-    verifierordrecroissant(board,n);
-    board->casetrouver[n][0] = board->casetrouver[n-1][0];
-    board->casetrouver[n][1] = board->casetrouver[n-1][1]-1;
-}
-
-void droite(Board *board, int n) {
-    verifierordrecroissant(board,n);
-    board->casetrouver[n][0] = board->casetrouver[n-1][0];
-    board->casetrouver[n][1] = board->casetrouver[n-1][1] + 1;
-}
-
-void bas(Board *board, int n) {
-    verifierordrecroissant(board,n);
-    board->casetrouver[n][0] = board->casetrouver[n-1][0]+1;
-    board->casetrouver[n][1] = board->casetrouver[n-1][1];
-}
-
-void demandeDeplacement(Board *board, int n) {
-    char direction;
-    scanf("%c", &direction);
-    switch(direction) {
-        case 'z':
-            haut(board, n);
-            break;
-        case 'q':
-            gauche(board, n);
-            break;
-        case 's':
-            bas(board, n);
-            break;
-        case 'd':
-            droite(board, n);
-            break;
-        case'e':
-            for(int i =0; i<n;i++){
-                printf("%d \n",board->casetrouver[i][3]);
-            }
-
-        default:
-            printf("Mauvaise entree, veuillez entrer z, q, s ou d.\n");
-            demandeDeplacement(board, n);
-            break;
+        }
+        else{
+            printf("plus grand");
+        }
 
 
     }
+
+    if (direction == 'd') {
+        //droite
+        printf("actuellement %d  et %d ", memoire[indice][2], board->GRID[x][y+1]);
+        if (memoire[indice][2] < board->GRID[x][y+1]) {
+            printf("plus petit");
+            indice++;
+            memoire[indice][0] = x;
+            memoire[indice][1] = y+1;
+            memoire[indice][2] = board->GRID[x][y+1];
+            board->GRID[x][y]=0;
+
+        } else {
+
+            printf("plus grand");
+        }
+
+
+    }
+
+
+    printf("la valeur n %d\n la valeur x %d\n la valeur y %d\n le chiffre  est %d\n ",indice,memoire[indice][0],indice,memoire[indice][1],indice,memoire[indice][2]);
 }
 
+//droite
 
 
-int move(Board *board, int n) {
-    n++;
+
+
+
+int move(Board *board) {
 
     //bas(board,n);
     //haut(board,n);
     //gauche(board,n);
     //droite(board,n);
-    demandeDeplacement(board,n);
-    int x=board->casetrouver[n][0];
-    int y=board->casetrouver[n][1];
-
-    setzero(board,x,y,n);
-
-    return n;
+    demandeDeplacement(board);
 }
-
 
 
 int main() {
     int n=0;
     char direction;
+    bool continuepartie = false;
     printf("Bienvenue au jeu Cardinal!\n");
 
     Board board;
     init_board(&board);
 
-    init_level1(&board);
-
     printf("\nManche 1\n");
-    printboard(board.grid);
+    printboard(&board);
+    init_level1(&board);
+    printboard(&board);
 
-
-    while(1){
-        n=move(board.grid,n);
-        printboard(board.grid);
-        printf("\nManche 1\n");
-        printf("tableau a l'indice %d ",board.grid[n][3]);
-
-        printf("Les deplacements :\n pour aller en haut -> z \n pour aller en bas -> s \n pour aller a gauche -> q \n pour aller a droite -> d\n");
-        printf("Votre choix ? : ");
-
+    // Boucle pour les déplacements
+    while (continuepartie==false) {
+        printboardwithpos(&board, memoire[indice][0], memoire[indice][1]); // Affichage de la grille avec la case actuelle en couleur
+        demandeDeplacement(&board); // Demande de déplacement au joueur
+        continuepartie=estPartieTerminee(&board);
     }
 
-
+    // Affichage des cases visitées
+    printf("indice = %d\n",indice);
+    for(int i=0;i<indice;i++){
+        printf("La case visitée était à la ligne %d et colonne %d, la valeur était %d\n", memoire[i][0]+1, memoire[i][1]+1, memoire[i][2]);
+    }
+    continuepartie=false;
+    init_level2(&board);
+    while (continuepartie==false) {
+        printboardwithpos(&board, memoire[indice][0], memoire[indice][1]); // Affichage de la grille avec la case actuelle en couleur
+        demandeDeplacement(&board); // Demande de déplacement au joueur
+        continuepartie=estPartieTerminee(&board);
+    }
 
 
 
---
+++ main.h
@@ -0,0 +1,17 @@
+//
+// Created by Ecole on 03/04/2023.
+//
+
+#ifndef UNTITLED4_MAIN_H
+#define UNTITLED4_MAIN_H
+
+
+#define X 9
+#define Y 9
+
+typedef struct {
+    int GRID[X][Y];
+    int N;
+    int CASETROUVER[X*Y][3];}
+        Board;
+#endif //UNTITLED4_MAIN_H
